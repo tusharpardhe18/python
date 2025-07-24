46b152c27020c6b062839db744a332cf3568c31a
tushar <tusharpardhe2018@gmail.com>
2025-07-25 02:39:35 +0530

    Day 1

---
+++ datatypes.py
@@ -0,0 +1,5 @@
+a = 1 # integer
+b = 1.22 # floating number
+c = True # boolean
+d = "Tushar" # string
+e = None # none
---
+++ operators.py
@@ -0,0 +1,35 @@
+# arthematic operators
+a = 34
+b = 4
+
+c = a + b
+# print(c) 
+
+d = a - b
+# print(d) 
+
+e = a * b
+# print(e)
+
+f = a / b
+# print(f)
+
+# Assignement Operators
+
+a = 4-2
+# print(a)
+
+b = 6
+b += 3
+# print (b)
+
+# Comparison Operators
+
+b = 5!=4
+# print(b)
+
+
+# Logical Operators
+
+e = False or True
+print(e)
