18578851efdd3691aad66bd17bc1189b66f6b03b
tushar <tusharpardhe2018@gmail.com>
2025-07-25 18:28:37 +0530

    Day 2

--- lists.py
+++ lists.py
@@ -11,8 +11,16 @@ L1 = [7,9,"harry"]
 
 L1[0] #7
 L1[1] #9
-L1[70] #error
+# L1[70] #error
 L1[0:2] #[7,9] -> list slicing 
+
+friends = ["Apple", "Orange", 5, 345.06, False, "Aaskash", "Rohan"]
+
+print(friends[0])
+friends[0] = "Grapes" # unline strings, lists are mutable
+
+print(friends[0])
+print(friends[1:4])
 
 # List Methods
 '''
@@ -21,8 +29,25 @@ L1 = [1,8,7,2,21,15]
 
 - L1.sort(): update the list to [1,2,7,8,15,21]
 - L1.reverse(): updates the list to [15,21,2,7,8,1]
-- L1. append
+- L1.append(8): adds 8 at the end of the list
+- L1.insert(3,8): this will add 8 at 3rd index
+- L1.pop(2): will delete the element at index 2 and return its value
+- L1.remove(21): this will remove 21 fromt he list
 
+'''
+
+# Tuples
+'''
+the tuple in an immutable data type in python
+a = () -> empty table
+a = (1, ) -> tuple with only one element needs a comma
+a = (1,7,2) -> tuple with more than one element
+
+'''
+
+# Tuple Methods
+'''
+Consider the following tuple.
 
 
 '''
