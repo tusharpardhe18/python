4728883913208a58db7c8f06a33b33e302df9193
tushar <tusharpardhe2018@gmail.com>
2025-07-28 16:50:44 +0530

    Daay 4

---
+++ function.py
@@ -0,0 +1,77 @@
+# Function :
+'''
+A function is a named block of code that performs a specific task. You define it once, and use (or "call") it as many times as needed.
+
+Why Use Functions?
+
+- Avoid repetition
+
+- Make code readable and organized
+
+- Test and debug smaller parts easily
+
+- Separate logic from execution
+
+Function Basics
+'''
+#  Defining a Function :
+
+def greet():
+    print("Hello!")
+
+# Calling a Function :
+
+greet() # Hello!
+
+# Parameters and Arguments :
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Tushar") # Hello, Tushar!
+# You can pass any types like numbers, strings, lists, other functions, etc.
+
+# Return Values :
+
+# Use return to send back data from a function.
+def add(a, b):
+    return a + b
+
+result = add(3, 4)
+print(result) # 7
+# if you don’t use return, the function returns None by default.
+
+# Default Parameters :
+
+# you can set default values for parameters.
+def greet(name="stranger"):
+    print(f"Hello, {name}!")
+
+greet()           # Hello, stranger!
+greet("Alice")    # Hello, Alice!
+
+# Variable Scope :
+
+# local scope :
+# variables declared inside a function exist only inside it
+def f():
+    x = 5  # local to f()
+    print(x)
+
+f() # 5
+# print(x)   ❌ Error: x is not defined
+
+# global scope :
+# variables outside any function are global. You can access them inside functions, but to modify them, use global.
+
+x = 10
+
+def f():
+    global x
+    x = 20
+
+f()
+print(x) # 20
+# avoid modifying globals—pass variables as arguments instead.
+
+
---
+++ function_questions.py
@@ -0,0 +1,43 @@
+# Practice: Build Your Intuition
+
+# Easy
+# 1. Write a function to calculate the factorial of a number.
+
+# def fact(x):
+#     f=1
+#     for i in range(1,x+1):
+#         f *= i 
+#     print(f)
+# x = int(input("Enter a number: "))
+# fact(x)
+
+
+# 2. Write a function that returns the maximum of three numbers.
+
+# def maximum(a,b,c):
+#     m = max(a,b,c)
+#     print(f"{m} is maximum") 
+
+# a = int(input("Enter number 1: "))
+# b = int(input("Enter number 2: "))
+# c = int(input("Enter number 3: "))
+# maximum(a,b,c)
+
+# 3. Write a function that checks if a string is a palindrome.
+
+# def palindrome(x):
+#     v=x
+#     r=0
+#     while x>0:
+#         r=r*10+x%10
+#         x//=10
+#     if v==r:
+#         print("palindrome")
+#     else:
+#         print("not a palindrome")
+
+# palindrome(121)
+
+# 4. Write a function that counts vowels in a string.
+
+# 5. Write a function to check if a number is prime.
